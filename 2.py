ed0c23b62749fc4905608faa44f30032fdacdfc6
Shubham Bhatnagar <39576123+shubhambhatnag@users.noreply.github.com>
2023-12-02 00:57:54 -0500

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,17 @@
+filename = '2.txt'
+file = open("inputs/"+filename, 'r')
+
+with file:
+    powerCount = 0
+    for line in file.readlines():
+        colorCount = {"red":-1, "green":-1, "blue":-1}
+        game = line.rstrip().split(": ")[1]
+        checkPossible = True
+        for colorSet in game.split("; "):
+            for color in colorSet.split(", "):
+                turn = color.split(" ")
+                if (int(turn[0]) > colorCount[turn[1]]):
+                    colorCount[turn[1]] = int(turn[0])
+        
+        powerCount += int(colorCount["red"] * colorCount["green"] * colorCount["blue"])
+    print(powerCount)
