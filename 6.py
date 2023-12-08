d15912dccc2950e8caf1664b83c8fc7b511611f7
shubhambhatnag <shubhambhatnagar2017@gmail.com>
2023-12-07 22:38:32 -0500

    6 in prog

---
+++ 6.py
@@ -0,0 +1,31 @@
+filename = '6.txt'
+file = open("inputs/"+filename, 'r')
+
+
+races = {}
+lines = file.readlines()
+times = lines[0].rstrip().split(" ")[1:]
+#delete all '' from times
+while ('' in times):
+    times.remove('')
+
+distances = lines[1].rstrip().split(" ")[1:]
+while ('' in distances):
+    distances.remove('')
+print(times)
+print(distances)
+
+# map each time to each distance
+
+for i in range(len(times)):
+    races[int(times[i])] = int(distances[i])
+total = 1
+for time in races:
+    record = races[time]
+    workingTimes = 0
+    for i in range(time+1):
+        if ((i * (time - i)) > record):
+            workingTimes += 1
+    print(workingTimes)
+    total *= workingTimes
+print(total)
